19bf4d7d2ce9613e0ca43cfddf17f80499f18734
Karthik Chandraraj <ckarthik17@gmail.com>
2013-04-25 13:20:56 +0530

    CK\Sriram : disabling next and previous

--- src/com/amnesty/panicbutton/spike/ScreenSlideActivity.java
+++ src/com/amnesty/panicbutton/spike/ScreenSlideActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,22 +17,37 @@ import java.util.List;
 
 public class ScreenSlideActivity extends FragmentActivity {
     private ViewPager viewPager;
+    private Button previousButton;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wizard);
 
+        previousButton = (Button) findViewById(R.id.previous);
+        nextButton = (Button) findViewById(R.id.next);
+
         viewPager = (ViewPager) findViewById(R.id.pager);
-        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager());
+        final PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pageAdapter);
+        previousButton.setEnabled(false);
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                previousButton.setEnabled(position != 0);
+                nextButton.setEnabled(position != (pageAdapter.getCount() - 1));
+            }
+        });
     }
 
-    public void showNext(View view) {
+    public void next(View view) {
         viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
     }
 
-    public void showPrevious(View view) {
+    public void previous(View view) {
         viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
     }
 
